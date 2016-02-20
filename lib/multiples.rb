96e4a028ed54a6ac28feecac59605ce4f3b1c9fe
joketrump <joketrump@gmail.com>
2016-02-20 19:56:44 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+# limit = 10
+def collect_multiples(limit)
+  multiples = Array.new
+  counter =  1
+  while counter < limit
+    if counter % 3 == 0 || counter % 5 == 0
+      multiples.push(counter)
+    end
+    counter += 1
+  end
+  return multiples
+end
+
+def sum_multiples(limit)
+  sum = 0
+  collect_multiples(limit).each { |multiple| sum += multiple}
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,32 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+  def initialize(limit)
+      @limit = limit
+  end
+#   def limit=(limit)
+#     @limit = limit
+#   end
+#   def limit
+#     @limit
+#   end
+
+  def collect_multiples
+    multiples = Array.new
+    counter = 1
+    while counter < @limit
+      if counter % 3 == 0 || counter % 5 == 0
+        multiples.push(counter)
+      end
+      counter += 1
+    end
+    return multiples
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.each { |multiple| sum += multiple}
+    return sum
+  end
+
+end
